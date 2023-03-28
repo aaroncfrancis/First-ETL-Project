9ca23cdc2e1c4a453d6768bc4a872c5898b407ac
Aaron Francis <aaronfrancis99@gmail.com>
2023-03-28 00:20:17 -0400

    plot added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly_express as px
+import pandas_datareader as fin
 df = pd.read_csv('all_stocks_5yr.csv')
 
 """data checks"""
@@ -21,13 +23,21 @@ print(df.tail(2))
 # df = df.drop('volume', axis=1)
 
 """or use:"""
-df = df.drop(columns=['volume','high', 'low'])
-print(df.head(1))
+# df = df.drop(columns=['volume','high', 'low'])
 
 """rearrange columns"""
 df = df[['Name','date','open','close']]
-print(df.head(1))
 
 """find the change in stock price by taking doing close - open"""
-df['Net'] = df['close'] - df['open']
+df['net'] = df['close'] - df['open']
 print(df.head(1))
+
+"""create new csv"""
+df.to_csv('s&p500 cleaned',index=False)
+
+#----step2----#
+
+"""plot the data"""
+figure = px.line(title='Stock Chart')
+figure.add_scatter(x=df.index, y=df['net'], mode='lines')
+figure.show()
